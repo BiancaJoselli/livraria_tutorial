e156530e23f35de49c28e6ac971a3a5c09a1e2ac
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-02-26 11:54:20 -0300

    FEAT: Criação da editora na livraria

--- app/urls.py
+++ app/urls.py
@@ -7,12 +7,13 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import CategoriaViewSet, UserViewSet 
+from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet
 
 
 router = DefaultRouter()
 
 router.register(r'categorias', CategoriaViewSet) 
+router.register(r'editora', EditoraViewSet)
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
--- core/views/categoria.py
+++ core/views/categoria.py
@@ -4,5 +4,5 @@ from core.models import Categoria
 from core.serializers import CategoriaSerializer
 
 class CategoriaViewSet(ModelViewSet):
-    queryset = Categoria.objects.all()
+    queryset = Categoria.objects.order_by('descricao')
     serializer_class = CategoriaSerializer
